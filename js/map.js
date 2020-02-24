eccb1932b6cae07e6b744cab858992c11d24b243
cactusjam <alfimova.evg@gmail.com>
2020-02-24 22:08:04 +0200

    js карта

--- js/map.js
+++ js/map.js
@@ -1,29 +1,22 @@
-var link = document.querySelector(".btn_feedback");
-var popup = document.querySelector(".modal_feedback");
-var close = document.querySelector(".modal_close");
-var login = popup.querySelector("[name=user_name]");
-var form = popup.querySelector("form");
-var email = popup.querySelector("[name=user_email]")
+var mapLink = document.querySelector(".button_map");
+var mapPopup = document.querySelector(".modal_map");
+var mapClose = mapPopup.querySelector(".modal_close");
 
-link.addEventListener("click", function(evt) {
+mapLink.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.add("modal_show");
-    login.focus();
+    mapPopup.classList.add("modal_show");
 });
 
-close.addEventListener("click", function (evt) {
+mapClose.addEventListener("click", function (evt) {
     evt.preventDefault();
-    popup.classList.remove("modal_show");
+    mapPopup.classList.remove("modal_show");
 });
 
 window.addEventListener("keydown", function (evt) {
     if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal_show")) {
-        popup.classList.remove("modal_show");
-      }
+        if (mapPopup.classList.contains("modal_show")) {
+            evt.preventDefault();
+            mapPopup.classList.remove("modal_show");
+        }
     }
-  });
-
-
-
+});
