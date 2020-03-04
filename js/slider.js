edbe1a09202111339d97f55067ef3df0e36da8a7
cactusjam <alfimova.evg@gmail.com>
2020-03-04 12:05:29 +0200

    slider

--- js/slider.js
+++ js/slider.js
@@ -1,17 +1,16 @@
 
-// var sliderControl = document.querySelectorAll(".slider_input");
-// var slider = document.querySelectorAll(".slider_item");
-// var currentSlider = document.querySelector ('active')
+var sliderControl = document.querySelectorAll(".slider_input");
+var slider = document.querySelectorAll(".slider_item");
 
-// for (let i = 0; i < slider.length; i++) {
-//     sliderControl[i].addEventListener('click', function () {
-//       if (sliderControl[i].checked) {
-//         slider[i].classList.add("active");
-//       };
-//       for (let i = 0; i < slider.length; i++)  {
-//         if (!sliderControl[i].checked) {
-//           slider[i].classList.remove("active");
-//         };
-//       }
-//     });
-//   };
+for (let i = 0; i < slider.length; i++) {
+    sliderControl[i].addEventListener('click', function () {
+      if (sliderControl[i].checked) {
+        slider[i].classList.add("active");
+      };
+      for (let i = 0; i < slider.length; i++)  {
+        if (!sliderControl[i].checked) {
+          slider[i].classList.remove("active");
+        };
+      }
+    });
+  };
