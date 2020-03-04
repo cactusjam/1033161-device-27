46853e9fbbe0595e8e354e19e377c67120144ff9
cactusjam <alfimova.evg@gmail.com>
2020-03-04 15:04:08 +0200

    slider tabs работающий

--- js/slider.js
+++ js/slider.js
@@ -1,7 +1,7 @@
 
 var sliderControl = document.querySelectorAll(".slider_input");
 var slider = document.querySelectorAll(".slide_item");
-var tabsControl = document.querySelector(".tabs_btn");
+var tabsControl = document.querySelectorAll(".tabs_nav .btn");
 var tabs = document.querySelectorAll(".tab_content");
 
 
@@ -22,11 +22,11 @@ for (let i = 0; i < slider.length; i++) {
       tabsControl[i].addEventListener('click', function() {
           for (let i = 0; i < tabs.length; i++) {
             if (tabs[i].classList.contains("active")) {
-                tabs[1].classList.remove("active");
+                tabs[i].classList.remove("active");
                 tabsControl[i].classList.remove("btn_active")
           };
         };
         tabsControl[i].classList.add('btn_active');
         tabs[i].classList.add("active");
-      })
-  }
+      });
+  };
