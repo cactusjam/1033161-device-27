fb5230adc5bb85b2f96c8e1c9fa70eb118f5d991
cactusjam <alfimova.evg@gmail.com>
2020-03-09 19:18:31 +0200

    оживление слайдера

--- js/slider.js
+++ js/slider.js
@@ -6,29 +6,41 @@ var tabsControl = document.querySelectorAll(".tabs_nav .btn");
 var tabs = document.querySelectorAll(".tab_content");
 
 
-sliderControl.forEach(function (element) {
-  element.addEventListener('click', function () {
-    let dataSlide = element.getAttribute('data-slide');
-    slider.forEach(function (element) {
-      if (dataSlide === element.getAttribute('data-slide')) {
-        element.classList.remove('hidden');
+if (typeof NodeList.prototype.forEach !== 'function') {
+  NodeList.prototype.forEach = Array.prototype.forEach;
+}  
+
+sliderControl.forEach(function (component) {
+  component.addEventListener('click', function () {
+    let dataSlider = component.getAttribute('data-slide');
+    slider.forEach(function (component) {
+      if (dataSlider === component.getAttribute('data-slide')) {
+        component.classList.remove('covered');
       } else {
-        element.classList.add('hidden');
+        component.classList.add('covered');
       }
     });
+    sliderControl.forEach(function (component) {
+      component.classList.remove('current_btn');
+    });
+      component.classList.add('current_btn');
   });
 });
 
 
-  for (let i = 0; i < tabs.length; i++) {
-      tabsControl[i].addEventListener('click', function() {
-          for (let i = 0; i < tabs.length; i++) {
-            if (tabs[i].classList.contains("active")) {
-                tabs[i].classList.remove("active");
-                tabsControl[i].classList.remove("btn_active")
-          };
-        };
-        tabsControl[i].classList.add('btn_active');
-        tabs[i].classList.add("active");
-      });
-  };
+tabsControl.forEach(function (component) {
+  component.addEventListener('click', function () {
+    let dataSlider = component.getAttribute('data-slide');
+    tabs.forEach(function (component) {
+      if (dataSlider === component.getAttribute('data-slide')) {
+        component.classList.remove('covered');
+      } else {
+        component.classList.add('covered');
+      }
+    });
+      tabsControl.forEach(function (component) {
+      component.classList.remove('btn_active');
+    });
+      component.classList.add('btn_active');
+  });
+});
