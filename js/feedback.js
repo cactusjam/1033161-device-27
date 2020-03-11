10815576ae8298d9c3a7b2d8be45938cc9ac045d
cactusjam <alfimova.evg@gmail.com>
2020-03-11 13:03:38 +0200

    автопрефиксер

--- js/feedback.js
+++ js/feedback.js
@@ -6,34 +6,33 @@ var login = popup.querySelector("[name=user_name]");
 var email = popup.querySelector("[name=user_email]");
 var message = popup.querySelector("[name=user_message]")
 
-link.addEventListener("click", function(evt) {
-    evt.preventDefault();
-    popup.classList.add("modal_show");
-    login.focus();
+link.addEventListener("click", function (evt) {
+	evt.preventDefault();
+	popup.classList.add("modal_show");
+	login.focus();
 });
 
 close.addEventListener("click", function (evt) {
-    evt.preventDefault();
-    popup.classList.remove("modal_show");
-    popup.classList.remove("modal_error");
+	evt.preventDefault();
+	popup.classList.remove("modal_show");
+	popup.classList.remove("modal_error");
 });
 
-form.addEventListener("submit", function(evt) {
-  if (!login.value || !email.value || !message.value) {
-    evt.preventDefault();
-    popup.classList.remove("modal_error");
-    popup.offsetWidth = popup.offsetWidth; 
-    popup.classList.add("modal_error"); 
-  }
+form.addEventListener("submit", function (evt) {
+	if (!login.value || !email.value || !message.value) {
+		evt.preventDefault();
+		popup.classList.remove("modal_error");
+		popup.offsetWidth = popup.offsetWidth;
+		popup.classList.add("modal_error");
+	}
 });
 
 window.addEventListener("keydown", function (evt) {
-    if (evt.keyCode === 27) {
-      evt.preventDefault();
-      if (popup.classList.contains("modal_show")) {
-        popup.classList.remove("modal_show");
-        popup.classList.remove("modal_error"); 
-      }
-    }
-  });
-
+	if (evt.keyCode === 27) {
+		evt.preventDefault();
+		if (popup.classList.contains("modal_show")) {
+			popup.classList.remove("modal_show");
+			popup.classList.remove("modal_error");
+		}
+	}
+});
--- js/slider.js
+++ js/slider.js
@@ -1,4 +1,3 @@
-
 var sliderControl = document.querySelectorAll(".slider_btn");
 var slider = document.querySelectorAll(".slide_item");
 
@@ -7,40 +6,40 @@ var tabs = document.querySelectorAll(".tab_content");
 
 
 if (!NodeList.prototype.forEach) {
-  NodeList.prototype.forEach = Array.prototype.forEach;
-} 
+	NodeList.prototype.forEach = Array.prototype.forEach;
+}
 
 sliderControl.forEach(function (component) {
-  component.addEventListener('click', function () {
-    let dataSlider = component.getAttribute('data-slide');
-    slider.forEach(function (component) {
-      if (dataSlider === component.getAttribute('data-slide')) {
-        component.classList.remove('covered');
-      } else {
-        component.classList.add('covered');
-      }
-    });
-    sliderControl.forEach(function (component) {
-      component.classList.remove('current_btn');
-    });
-      component.classList.add('current_btn');
-  });
+	component.addEventListener('click', function () {
+		let dataSlider = component.getAttribute('data-slide');
+		slider.forEach(function (component) {
+			if (dataSlider === component.getAttribute('data-slide')) {
+				component.classList.remove('covered');
+			} else {
+				component.classList.add('covered');
+			}
+		});
+		sliderControl.forEach(function (component) {
+			component.classList.remove('current_btn');
+		});
+		component.classList.add('current_btn');
+	});
 });
 
 
 tabsControl.forEach(function (component) {
-  component.addEventListener('click', function () {
-    let dataSlider = component.getAttribute('data-slide');
-    tabs.forEach(function (component) {
-      if (dataSlider === component.getAttribute('data-slide')) {
-        component.classList.remove('covered');
-      } else {
-        component.classList.add('covered');
-      }
-    });
-      tabsControl.forEach(function (component) {
-      component.classList.remove('btn_active');
-    });
-      component.classList.add('btn_active');
-  });
+	component.addEventListener('click', function () {
+		let dataSlider = component.getAttribute('data-slide');
+		tabs.forEach(function (component) {
+			if (dataSlider === component.getAttribute('data-slide')) {
+				component.classList.remove('covered');
+			} else {
+				component.classList.add('covered');
+			}
+		});
+		tabsControl.forEach(function (component) {
+			component.classList.remove('btn_active');
+		});
+		component.classList.add('btn_active');
+	});
 });
